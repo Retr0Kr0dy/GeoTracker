f1f204ed13618b7846041db988a2df72ba543534
Rick Astley <never.gonna@giveu.up>
2023-05-31 14:58:12 +0200

    Merging v0.1.2 from [...]

---
+++ server.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib
+import ssl 
+from sys import exit 
+
+addr        = '0.0.0.0'
+port        = 8000
+keyfile     = 'key.pem'
+certfile    = 'cert.pem'
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if (self.path == '/'):
+            self.path = '/index.html'
+
+        else:
+            self.send_response(555)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+
+            self.wfile.write(bytes("it isn't that easy...", "utf-8"))
+            exit(0)
+        
+        try:        
+            f = open(self.path[1:]).read()
+        except:
+           f = "File not found"
+        
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        self.wfile.write(bytes(f, "utf-8"))
+    
+    def do_POST(self):
+        if (self.path == '/locate.php'):
+            content_length  = int(self.headers['Content-Length'])
+            post_data       = self.rfile.read(content_length).decode('utf-8')
+            post_data       = urllib.parse.parse_qs(post_data)
+
+            lon = post_data['lon'][0]
+            lat = post_data['lat'][0]
+
+            self.send_response(666)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+
+            self.wfile.write(bytes("demon", "utf-8"))
+            print(f"{lon},{lat}")
+        else:
+            self.send_response(555)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+
+            self.wfile.write(bytes("it isn't that easy...", "utf-8"))
+            
+def main():
+    httpd = HTTPServer((addr, port), handler)
+    httpd.socket = ssl.wrap_socket(httpd.socket, keyfile=keyfile, certfile=certfile, server_side=True)
+    print(f'Server running on {addr}:{port}...')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    exit(main())
